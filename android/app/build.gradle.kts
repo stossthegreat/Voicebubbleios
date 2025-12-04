be0fcb6ac1dfbd8816788548f5780f7a74c1f7d0
VoiceBubble Dev <dev@voicebubble.app>
2025-12-04 03:07:47 +0000

    chore: Bump Android versionCode from 2 to 3
    Updated for new build with latest fixes:
    - Firebase fully configured
    - Paywall free trial skip fixed

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.voicebubble.app"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0.0"
     }
 
